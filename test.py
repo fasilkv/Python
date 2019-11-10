7da1e2e17bffa090f75e713533d191b4c0258063
fasilkv <fasilkv000@gmail.com>
2019-11-10 14:33:58 +0530

    code added test

---
+++ test.py
@@ -0,0 +1,16 @@
+print("o = open")
+print("e = exit")
+print("s = save")
+
+while True:
+    key = input("enter the key")
+    if key == 's':
+        print("save")
+
+    elif key == 'o':
+        print("open")
+    elif key == 'e':
+        print("exit")
+
+    else:
+        print("error")
